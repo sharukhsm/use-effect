5f3fdf616875c02513e0261f36e4f3f6ce748bdb
Sharukh <sharukhsm@gmail.com>
2021-11-10 15:38:17 +0530

    Every render useEffect

--- src/HookCounter.js
+++ src/HookCounter.js
@@ -1,12 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function HookCounter() {
   const [count, setCount] = useState(0);
+
   const handleClick = () => {
     setCount(count + 1);
   };
+
+  //This runs after every re-render
+  useEffect(() => {
+    document.title = `you clicked ${count} times`;
+  });
+
   return (
     <div>
+      <h1>{count}</h1>
       <button onClick={handleClick}>Increase</button>
     </div>
   );
