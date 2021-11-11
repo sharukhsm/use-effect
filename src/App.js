651a0e43a10fc1132ea6194fbc64863d36165882
Sharukh <sharukhsm@gmail.com>
2021-11-11 11:46:27 +0530

    Complete DataFetching

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import HookCounter2 from "./HookCounter2";
 import HookCounter1 from "./HookCounter1";
 import HookMouse from "./HookMouse";
+import DataFetching from "./DataFetching";
 function App() {
   return (
     <div className="App">
       {/* <HookCounter2 /> */}
       {/* <HookCounter1 /> */}
-      <HookMouse />
+      {/* <HookMouse /> */}
+      <DataFetching />
     </div>
   );
 }
---
+++ src/DataFetching.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function DataFetching() {
+  const [apiData, setApiData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        //Setting the response to a variable
+        setApiData(res.data);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []); //Empty dependency, so that It only renders once.
+
+  return (
+    <div>
+      <ul>
+        {apiData.map((datum) => (
+          <li key={datum.id}> {datum.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default DataFetching;
